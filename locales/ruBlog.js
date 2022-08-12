b7922390af77ffefa99a8a4f0a5854febf30bd94
G-Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-08-13 02:35:32 +0400

    t

--- locales/ruBlog.js
+++
@@ -1,5 +0,0 @@
-export default {
-  title: "Blog",
-  blogText: "Bukhaidze I Turn 6 our new project",
-  blogDate: "November 23. 2021",
-};
--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/index.css'
-import '../styles/applications.scss'
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
