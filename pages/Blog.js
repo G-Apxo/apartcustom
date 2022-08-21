c079ccb08821511c5118bc9340643ee6890c89c7
G-Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-08-21 22:18:57 +0400

    Merge branch 'main' into adminPanel

--- pages/Blog.js
+++ pages/Blog.js
@@ -20,70 +20,16 @@ export default function IndexPage() {
       <div className="hero">
         <Nav />
         <div>
-          <Container>
+          <Container className="mb-120">
             <h2 className="row-marginer mt-120 mb-60">Blog</h2>
             <Row>
               <Link className="blog-single-blog" href="/singleblog">
                 <Col className="cursor mt-5" xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-                  <div className="image__box blog__1">
+                  <div className="">
                     <div className="blog-image__content">
                       {/* ar mushaobs css */}
-                      <p>November 23. 2021</p>
-                      <h2>Bukhaidze I Turn 6 our new project</h2>
-                    </div>
-                  </div>
-                </Col>
-              </Link>
-              <Link className="blog-single-blog" href="/singleblog">
-                <Col className="cursor mt-5" xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-                  <div className="image__box blog__2">
-                    <div className="blog-image__content">
-                      <p>November 23. 2021</p>
-                      <h2>Bukhaidze I Turn 6 our new project</h2>
-                    </div>
-                  </div>
-                </Col>
-              </Link>
-            </Row>
-            <Row>
-              <Link className="blog-single-blog" href="/singleblog">
-                <Col className="cursor mt-5" xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-                  <div className="image__box blog__3">
-                    <div className="blog-image__content">
-                      <p>November 23. 2021</p>
-                      <h2>Bukhaidze I Turn 6 our new project</h2>
-                    </div>
-                  </div>
-                </Col>
-              </Link>
-              <Link className="blog-single-blog" href="/singleblog">
-                <Col className="cursor mt-5" xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-                  <div className="image__box blog__4">
-                    <div className="blog-image__content">
-                      <p>November 23. 2021</p>
-                      <h2>Bukhaidze I Turn 6 our new project</h2>
-                    </div>
-                  </div>
-                </Col>
-              </Link>
-            </Row>
-            <Row>
-              <Link href="/singleblog">
-                <Col className="cursor mt-5" xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-                  <div className="image__box blog__5">
-                    <div className="blog-image__content">
-                      <p>November 23. 2021</p>
-                      <h2>Bukhaidze I Turn 6 our new project</h2>
-                    </div>
-                  </div>
-                </Col>
-              </Link>
-              <Link href="/singleblog">
-                <Col className="cursor mt-5" xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-                  <div className="image__box blog__6">
-                    <div className="blog-image__content">
-                      <p>November 23. 2021</p>
-                      <h2>Bukhaidze I Turn 6 our new project</h2>
+                      <p className="text-dark">November 23. 2021</p>
+                      <h2 className="text-dark">Bukhaidze I Turn 6 our new project</h2>
                     </div>
                   </div>
                 </Col>
