4e6077629e7dde14d717fa9c1d24b5f341931d22
G-Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-09-07 01:36:53 +0400

    all pages update

--- pages/ezo.js
+++ pages/ezo.js
@@ -53,26 +53,26 @@ export default function IndexPage2() {
           </Row>
           <div className="mob__view">
             <Row className="justify-content-between mt-5">
-              <Col xs="12" lg="2" md="2" sm="2" xl="2" xxl="2" className="mb-5">
+              <Col xs="12" lg="12" md="12" sm="12" xl="2" xxl="2" className="mb-5">
                 <Image src={Taghi} className="Taghi" />
               </Col>
-              <Col xs="6" lg="2" md="2" sm="2" xl="2" xxl="2">
+              <Col xs="6" lg="6" md="6" sm="6" xl="2" xxl="2">
                 <hr />
                 <p className="basic-texts-14">{t.treePlanted}</p>
                 <h3 className="projects-floor-yard">{t.treeNum}</h3>
               </Col>
-              <Col xs="6" lg="3" md="3" sm="3" xl="3" xxl="3">
+              <Col xs="6" lg="6" md="6" sm="6" xl="3" xxl="3">
                 <hr />
                 <p className="basic-texts-14">{t.exhibitionSpace}</p>
                 <h3 className="projects-floor-yard">{t.esm}</h3>
               </Col>
             </Row>
             <Row className="justify-content-between mt-5">
-              <Col xs="12" lg="4" md="4" sm="4" xl="4" xxl="4" className="mt-3">
+              <Col xs="12" lg="12" md="12" sm="12" xl="4" xxl="4" className="mt-3">
                 <h3 className="projects-floor-title"> {t.chooseApart}</h3>
                 <p>{t.chooseFloor}</p>
               </Col>
-              <Col xs="12" lg="4" md="4" sm="4" xl="4" xxl="4">
+              <Col xs="12" lg="12" md="12" sm="12" xl="4" xxl="4">
                 <Form.Select aria-label="Default select example">
                   <option>{t.chooseFloor}</option>
                   <option value="1">{t.chooseOne}</option>
@@ -136,15 +136,15 @@ export default function IndexPage2() {
           </Row>
           <form action="/send-data-here" method="post">
             <Row className=" mt-5 form-row align-items-center">
-              <Col xs="12" lg="3" md="3" sm="3" xl="3" xxl="3" className="mt-1">
+              <Col xs="12" lg="12" md="12" sm="12" xl="3" xxl="3" className="mt-1">
                 <label htmlFor="name"></label>
                 <input className="btn-test" type="text" id="name" name="name" placeholder={t.name} />
               </Col>
-              <Col xs="12" lg="3" md="3" sm="3" xl="3" xxl="3" className="mt-1">
+              <Col  xs="12" lg="12" md="12" sm="12" xl="3" xxl="3" className="mt-1">
                 <label htmlFor="email"></label>
                 <input className="btn-test" type="mail" id="email" name="email" placeholder={t.email} />
               </Col>
-              <Col xs="12" lg="3" md="3" sm="3" xl="3" xxl="3" className="mt-1">
+              <Col  xs="12" lg="12" md="12" sm="12" xl="3" xxl="3" className="mt-1">
                 <label htmlFor="tel"></label>
                 <input
                   className="btn-test"
@@ -156,13 +156,13 @@ export default function IndexPage2() {
                   required
                 />
               </Col>
-              <Col xs="12" lg="3" md="3" sm="3" xl="3" xxl="3" className="mt-1">
+              <Col  xs="12" lg="12" md="12" sm="12" xl="3" xxl="3" className="mt-1">
                 <Button className="btn-send">{t.req}</Button>
               </Col>
             </Row>
           </form>
           <Row className="justify-content-between">
-            <Col xs="12" lg="5" md="5" sm="5" xl="5" xxl="5">
+          <Col xs="12" lg="12" md="12" sm="12" xl="5" xxl="5">
               <h3>{t.aboutprj}</h3>
               <p className="projects--floor-about-text basic-texts-14">{t.apt}</p>
               <Col xs="12">
@@ -194,10 +194,10 @@ export default function IndexPage2() {
               </Col>
             </Col>
             <Col
-              xs="12"
-              lg="6"
-              md="6"
-              sm="6"
+               xs="12"
+               lg="12"
+               md="12"
+               sm="12"
               xl="6"
               xxl="6"
               className="d-flex justify-content-center align-items-center"
