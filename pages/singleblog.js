6c76686707ffda89b1cc13f1c032834a191d468f
G-Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-09-15 00:02:36 +0400

    api update

--- pages/singleblog.js
+++ pages/singleblog.js
@@ -29,7 +29,7 @@ export default function IndexPage() {
   const blogDesc = () => {
     var config = {
       method: "get",
-      url: "https://3bfd-95-137-233-63.ngrok.io/api/blog/ge/new-complex-from-apart",
+      url: "https://ce1f-95-137-233-63.ngrok.io/api/blog/ge/new-complex-from-apart",
       headers: {
         Authorization: "Basic YXBhcnRfdXNlcm5hbWU6YXBhcnRfcGFzc3dvcmRfYnJhZ3p5",
       },
