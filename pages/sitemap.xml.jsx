e786e7844382f45c39ce01f659bb1e7715c32002
Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-11-15 02:22:36 +0400

    Update sitemap.xml.jsx

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -5,12 +5,35 @@ const Sitemap = () => {
 };
 
 export const getServerSideProps = async ({ res }) => {
-    const BASE_URL = 'http://localhost:3000';
+    const BASE_URL = 'https://apart.ge';
 
     const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      // our URL's will go here
+    <urlset
+          xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+    
+    
+    <url>
+      <loc>https://apart.ge/</loc>
+      <lastmod>2022-11-14T22:21:55+00:00</lastmod>
+      <priority>1.00</priority>
+    </url>
+    <url>
+      <loc>https://apart.ge/invest-in-real-estate-in-tbilisi/</loc>
+      <lastmod>2022-11-14T22:21:55+00:00</lastmod>
+      <priority>0.80</priority>
+    </url>
+    <url>
+      <loc>https://apart.ge/projects/</loc>
+      <lastmod>2022-11-14T22:21:55+00:00</lastmod>
+      <priority>0.80</priority>
+    </url>
+    
+    
     </urlset>
   `;
 
