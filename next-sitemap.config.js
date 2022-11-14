a5847051af57e53e36176cf322b3212d7c92b193
Apxo <72552435+G-Apxo@users.noreply.github.com>
2022-11-15 02:12:53 +0400

    test

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://apart.ge/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
