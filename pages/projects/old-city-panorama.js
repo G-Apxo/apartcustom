ba050a7d6250484bfcd9dec30941bb1a75bef34f
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-12-08 13:53:24 +0400

    ocp 360 +

--- pages/projects/old-city-panorama.js
+++ pages/projects/old-city-panorama.js
@@ -167,14 +167,14 @@ export default function IndexPage() {
                                 {/* </Link> */}
                             </div>
                         </Col>
-                        {/* <Row className=' mt-40 mb-40 flex-helper'>
+                        <Row className=' mt-40 mb-40 flex-helper'>
                             <Col xs='6' lg='6' md='6' sm='6' xl='6' xxl='6'>
-                                {' '}
+                                {/* {' '}
                                 <a href='https://fasadi.netlify.app/fasadi/' target='_blank'>
                                     <i className=' cursor flex-helper'>
                                         {t.seeapart} <Image src={Vector} alt='arrow' className='arrow-prj' />
                                     </i>
-                                </a>
+                                </a> */}
                             </Col>
                             <Col xs='6' lg='6' md='6' sm='6' xl='6' xxl='6'>
                                 {' '}
@@ -184,7 +184,7 @@ export default function IndexPage() {
                                     </i>
                                 </a>
                             </Col>
-                        </Row> */}
+                        </Row>
                         <Row className='justify-content-between mt-5'>
                             <Col xs='3'>
                                 <hr />
